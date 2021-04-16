d7105922a60fbb06a6f645a5836060b87b809c2f
= <denholtz@washington.edu>
2021-04-16 13:09:22 -0700

    updated sparse-viz-query message

--- dbHandlers/dbPools.js
+++ dbHandlers/dbPools.js
@@ -7,4 +7,7 @@ module.exports.dataReadOnlyPool = new sql.ConnectionPool(dbConfig.dataRetrievalC
 // This connects as a user that has read and write access. Be careful with requests that use this pool
 module.exports.userReadAndWritePool = new sql.ConnectionPool(dbConfig.userTableConfig).connect();
 
-module.exports.mariana = new sql.ConnectionPool(dbConfig.mariana).connect();
+// module.exports.mariana = new sql.ConnectionPool(dbConfig.mariana).connect();
+module.exports.mariana = new sql.ConnectionPool(dbConfig.dataRetrievalConfig).connect();
+
+console.log('Reminder that Mariana is disconnected');
