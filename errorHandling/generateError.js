22947dd81e2afe80f4887a4c83e16a036afe4b33
Walker Malling <mallinwa@uw.edu>
2022-05-18 23:12:37 -0700

    fix error catching piped response after its sent
    clean up query handler

--- errorHandling/generateError.js
+++ errorHandling/generateError.js
@@ -1,28 +1,31 @@
 // Error mapping for queryHandler
 
 const errorCodeMap = {
-    ECANCEL: 'Request cancelled.',
-    EREQUEST: ''
-}
+  ECANCEL: "Request cancelled.",
+  EREQUEST: "",
+};
 
 module.exports = (errorObject) => {
-    let err;
+  let err;
 
-    switch(errorObject.code){
-        case 'ECANCEL':
-            err = 'The request was cancelled';
-            break;
+  switch (errorObject.code) {
+    case "ECANCEL":
+      err = "The request was cancelled";
+      break;
 
-        case 'EREQUEST':
-            err = (errorObject.originalError && errorObject.originalError.info) && errorObject.originalError.info.message;
-            break;
+    case "EREQUEST":
+      err =
+        errorObject.originalError &&
+        errorObject.originalError.info &&
+        errorObject.originalError.info.message;
+      break;
 
-        default:
-            err = 'An unknown error occured';
-            break;
-    }
+    default:
+      err = "An unknown error occured";
+      break;
+  }
 
-    if(!err) err = 'An unknown error occured';
+  if (!err) err = "An unknown error occured";
 
-    return err;    
-}
+  return err;
+};
--- utility/AccumulatorStream.js
+++ utility/AccumulatorStream.js
@@ -1,24 +1,24 @@
-const Transform = require('stream').Transform
+const Transform = require("stream").Transform;
 
 // Allows a larger buffer than default stream, reduces number of write operations
 
 module.exports = class AccumulatorStream extends Transform {
-    constructor(options){
-        super(options);
-        this._customBuffer = [];
-    }
+  constructor(options) {
+    super(options);
+    this._customBuffer = [];
+  }
 
-    _transform(chunk, encoding, done) {
-        this._customBuffer.push(chunk);
-        if(this._customBuffer.length >= 100){
-            this.push(this._customBuffer.join(''));
-            this._customBuffer = [];
-        }
-        done();
+  _transform(chunk, encoding, done) {
+    this._customBuffer.push(chunk);
+    if (this._customBuffer.length >= 100) {
+      this.push(this._customBuffer.join(""));
+      this._customBuffer = [];
     }
+    done();
+  }
 
-    _flush(done){
-        this.push(this._customBuffer.join('') + '\n');
-        done();
-    }
-}
+  _flush(done) {
+    this.push(this._customBuffer.join("") + "\n");
+    done();
+  }
+};
