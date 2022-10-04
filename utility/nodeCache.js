297bdd5e5bb3d2707383131da668591f4cd1ee4f
Walker Malling <mallinwa@uw.edu>
2022-10-04 15:49:15 -0700

    add logging to node cache in development

--- utility/nodeCache.js
+++ utility/nodeCache.js
@@ -1,3 +1,24 @@
 const NodeCache = require( "node-cache" );
+const logInit = require("../log-service");
 
-module.exports = new NodeCache();
+const isDevelopment =
+  process.env.NODE_ENV !== "production" || process.env.NODE_ENV !== "staging";
+
+// NOTE: no options are passed
+// see defaults: https://github.com/node-cache/node-cache#options
+// as is, the cache has no key limit, and unlimited TTL
+const cache = new NodeCache();
+
+if (isDevelopment) {
+  // log some basic cache usage
+  const log = logInit("nodeCache");
+  // https://github.com/node-cache/node-cache#events
+  cache.on("set", (key) => {
+    log.debug("node cache: set", { key })
+  });
+  cache.on("expired", (key) => {
+    log.debug("node cache: set", { key })
+  });
+}
+
+module.exports = cache;
