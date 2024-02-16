8426bb103e71b55d261c4f80e236aba4a25ea6c4
Walker <walker@anthanes.com>
2024-02-16 09:50:43 -0800

    update check name to check update conflicts

--- controllers/data-submission/check-name.js
+++ controllers/data-submission/check-name.js
@@ -10,8 +10,8 @@ const log = initializeLogger(
 );
 
 const checkSubmissionName = async (req, res) => {
-  const { id } = (req.user || {});
-  const { shortName, longName } = req.body;
+  const { id: userId } = (req.user || {});
+  const { shortName, longName, submissionId: targetSubmissionId } = req.body;
 
   const pool = await userReadAndWritePool;
 
@@ -19,24 +19,33 @@ const checkSubmissionName = async (req, res) => {
   let shortNameIsAlreadyInUse = false;
   let longNameIsAlreadyInUse = false;
   let folderExists = true;
+  // additional info
   let errors = [];
+  let shortNameUpdateConflict;
+  let longNameUpdateConflict;
 
-  // 1. check short name
+
+  if (!shortName) {
+    errors.push('No short name provided');
+  }
+  if (!longName) {
+    errors.push('No long name provided');
+  }
+
+  // 1. check if short name is already published
   if (shortName) {
     try {
       const checkNameRequest = await new sql.Request(pool);
       const sqlResponse = await checkNameRequest.query(`
-      select ID from tblDatasets
-      where Dataset_Name = '${shortName}'
-    `);
+        select ID from tblDatasets
+        where Dataset_Name = '${shortName}'
+      `);
+      // the short name is already published
       shortNameIsAlreadyInUse = Boolean(safePath(['recordset', '0', 'ID'])(sqlResponse));
     } catch (e) {
       log.error('sql error', { e });
       return res.sendStatus(500);
     }
-  } else {
-    shortNameIsAlreadyInUse = true;
-    errors.push ('No short name provided');
   }
 
   // 2. check long name
@@ -44,21 +53,23 @@ const checkSubmissionName = async (req, res) => {
     try {
       const checkLongNameRequest = await new sql.Request(pool);
       const longNameResponse = await checkLongNameRequest.query(`
-      select ID from tblData_Submissions
-      where Dataset_Long_Name = '${longName}'
-    `);
-      longNameIsAlreadyInUse = Boolean(safePath(['recordset', '0', 'ID'])(longNameResponse));
+        select ID, Submitter_ID from tblData_Submissions
+        where Dataset_Long_Name = '${longName}'
+      `);
+      let existingSubmissionId = safePath(['recordset', '0', 'ID'])(longNameResponse);
+      let existingSubmissionSubmitterId = safePath(['recordset', '0', 'Submitter_ID'])(longNameResponse);
+      longNameIsAlreadyInUse = Boolean(existingSubmissionId);
+
+      if (existingSubmissionId !== targetSubmissionId || existingSubmissionSubmitterId !== userId) {
+        longNameUpdateConflict = true;
+      }
     } catch (e) {
       log.error('sql error', { e });
       return res.sendStatus(500);
     }
-  } else {
-    longNameIsAlreadyInUse = true; // mark conflict
-    errors.push ('No long name provided');
   }
 
   // 3. check dropbox folder
-
   if (shortName) {
     try {
       await dropbox.filesListFolder({ path: `/${shortName}` });
@@ -67,31 +78,49 @@ const checkSubmissionName = async (req, res) => {
         log.info("folder not found", { responseStatus: e.status });
         folderExists = false;
       } else {
-        log.error("error getting folder ls", { ...e, userId: id });
+        log.error("error getting folder ls", { ...e, userId });
         return res.sendStatus(500);
       }
     }
   }
 
-  // 4. send results
+  //  4. ensure that if short name exists, it belongs to expected dataset
+  if (shortName) {
+    try {
+      const checkShortNameRequest = await new sql.Request(pool);
+      const resp = await checkShortNameRequest.query(`
+        select ID, Submitter_ID from tblData_Submissions
+        where Filename_Root = '${shortName}'
+      `);
+      const existingSubmissionId = safePath(['recordset', '0', 'ID'])(resp);
+      const existingSubmissionSubmitterId = safePath(['recordset', '0', 'Submitter_ID'])(resp);
+      if (existingSubmissionId !== targetSubmissionId || existingSubmissionSubmitterId !== userId) {
+        shortNameUpdateConflict = true;
+      }
+    } catch (e) {
+      log.error('sql error', { e });
+      return res.sendStatus(500);
+    }
+  }
 
-  log.info ("result of checkName", { errors, folderExists, shortNameIsAlreadyInUse, longNameIsAlreadyInUse, shortName, longName, userId: id });
+
+  // 5. send results
 
   const payload = {
     shortNameIsAlreadyInUse,
+    shortNameUpdateConflict,
     folderExists,
     longNameIsAlreadyInUse,
+    longNameUpdateConflict,
     errors,
     shortName,
     longName,
   }
 
-  if (folderExists || shortNameIsAlreadyInUse || longNameIsAlreadyInUse || errors.length) {
-    res.json ({ conflict: true, ...payload });
-  } else {
-    res.json ({ conflict: false, ...payload });
-  }
 
+  log.info ("result of checkName", { ...payload, userId });
+
+  res.json (payload);
 };
 
 module.exports = checkSubmissionName;
