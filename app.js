f8ab8b330380b850150c0616dfcc0d8680a8e46c
Howard Kim <hwk@uw.edu>
2025-04-28 10:07:36 -0700

    Revert "style on save"
    This reverts commit 12fdf7a79b0f67a22ffc885c456c9acd60272ff7.

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const passport = require('./middleware/passport');
-var useragent = require('express-useragent');
-const createNewLogger = require('./log-service');
-const webApp = require('./routes/webApp');
-const apiRouter = require('./routes/api');
-const dataRetrievalRoutes = require('./routes/dataRetrieval');
-const ApiCallDetails = require('./models/ApiCallDetail');
-const { v4: uuidv4 } = require('uuid');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const passport = require("./middleware/passport");
+var useragent = require("express-useragent");
+const createNewLogger = require("./log-service");
+const webApp = require("./routes/webApp");
+const apiRouter = require("./routes/api");
+const dataRetrievalRoutes = require("./routes/dataRetrieval");
+const ApiCallDetails = require("./models/ApiCallDetail");
+const { v4: uuidv4 } = require("uuid");
 
-const { monitor } = require('./mail-service/checkBouncedMail');
-const env = require('./config/environment');
+const { monitor } = require("./mail-service/checkBouncedMail");
+const env = require("./config/environment");
 
 // on startup, check for bounced mail
 monitor();
 
 const log = createNewLogger()
-  .setModule('app.js')
-  .addContext(['node_version', process.version]);
+  .setModule("app.js")
+  .addContext(["node_version", process.version]);
 
-log.info('starting app', {
+log.info("starting app", {
   DEBUG_USAGE: process.env.DEBUG_USAGE,
   DEBUG_USAGE_THROTTLE_MS: process.env.DEBUG_USAGE_THROTTLE_MS,
   CLUSTER_CHUNK_MAX_ROWS: process.env.CLUSTER_CHUNK_MAX_ROWS,
@@ -32,7 +32,7 @@ log.info('starting app', {
 const app = express();
 const port = process.env.PORT || 8080;
 
-process.on('warning', ({ name, message, stack }) => {
+process.on("warning", ({ name, message, stack }) => {
   log.warn(message, { name, stack });
 });
 
@@ -41,7 +41,7 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(passport.initialize());
 app.use(useragent.express());
 
@@ -49,7 +49,7 @@ app.use(useragent.express());
 app.use((req, res, next) => {
   let reqId = uuidv4();
   req.requestId = reqId;
-  res.set('X-CMAP-Request-Id', reqId);
+  res.set("X-CMAP-Request-Id", reqId);
   next();
 });
 
@@ -62,13 +62,13 @@ app.use((req, res, next) => {
 
 // Routes - DEPRECATED
 app.use(
-  '/dataretrieval',
-  passport.authenticate(['headerapikey', 'jwt'], { session: false }),
+  "/dataretrieval",
+  passport.authenticate(["headerapikey", "jwt"], { session: false }),
   dataRetrievalRoutes,
 );
 
 // API
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 // Web App
 app.use(webApp);
@@ -76,13 +76,13 @@ app.use(webApp);
 // Usage metrics logging
 app.use((req, res, next) => {
   // this will execute if neither the api nor webApp have already saved call details
-  req.cmapApiCallDetails.save(res, { caller: 'app' });
+  req.cmapApiCallDetails.save(res, { caller: "app" });
   next();
 });
 
 // start web server
 app.listen(port, () => {
-  log.info('api web server started', {
+  log.info("api web server started", {
     port,
     nodeEnv: env.NODE_ENV,
   });
