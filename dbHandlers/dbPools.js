b9a418a3bf3d67acc647c0aa167fde157b57cd9b
Howard Kim <hwk@uw.edu>
2025-05-20 12:19:17 -0700

    fix dbPool connection

--- dbHandlers/dbPools.js
+++ dbHandlers/dbPools.js
@@ -2,14 +2,30 @@ const sql = require('mssql');
 const dbConfig = require('../config/dbConfig');
 const Future = require('fluture');
 const { SERVER_NAMES } = require('../utility/constants');
+const logInit = require('../log-service');
+const log = logInit('dbPools');
 
 // Note, the mssql ConnectionPool connect method returns a promise
-const rainierReadOnly = new sql.ConnectionPool(
-  dbConfig.dataRetrievalConfig,
-).connect();
-const rainierReadWrite = new sql.ConnectionPool(
-  dbConfig.userTableConfig,
-).connect();
+const rainierReadOnly = new sql.ConnectionPool(dbConfig.dataRetrievalConfig)
+  .connect()
+  .catch((error) => {
+    log.error('Failed to connect to rainierReadOnly pool', { error });
+    return null;
+  });
+
+const rainierReadWrite = new sql.ConnectionPool(dbConfig.userTableConfig)
+  .connect()
+  .catch((error) => {
+    log.error('Failed to connect to rainierReadWrite pool', { error });
+    return null;
+  });
+
+const rossby = new sql.ConnectionPool(dbConfig.rossby)
+  .connect()
+  .catch((error) => {
+    log.error('Failed to connect to rossby pool', { error });
+    return null;
+  });
 
 const pools = {
   // This connects as a user that has no access to user details and can only read.
@@ -23,7 +39,7 @@ const pools = {
   // [SERVER_NAMES.mariana]: new sql.ConnectionPool(dbConfig.mariana).connect(),
   //
   // Rossby
-  [SERVER_NAMES.rossby]: new sql.ConnectionPool(dbConfig.rossby).connect(),
+  [SERVER_NAMES.rossby]: rossby,
   [SERVER_NAMES.rainier]: rainierReadOnly,
   rainierReadWrite: rainierReadWrite,
 };
