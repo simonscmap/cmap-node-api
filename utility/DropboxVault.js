00107c1c7dcd050e2918066acbcc5c0f4462803c
Howard Kim <hwk@uw.edu>
2025-07-03 15:46:22 -0700

    pause refresh token

--- utility/DropboxVault.js
+++ utility/DropboxVault.js
@@ -7,7 +7,7 @@ const auth = new Dropbox.DropboxAuth({
   clientId: process.env.DB_VAULT_APP_KEY,
   clientSecret: process.env.DB_VAULT_APP_SECRET,
   accessToken: process.env.DB_VAULT_ACCESS_TOKEN,
-  refreshToken: process.env.DB_VAULT_REFRESH_TOKEN,
+  // refreshToken: process.env.DB_VAULT_REFRESH_TOKEN,
 });
 
 const dbx = new Dropbox.Dropbox({
