169009df6db6df796309a0b2a9dd355970280f4d
Howard Kim <hwk@uw.edu>
2025-08-18 11:04:41 -0700

    wip cleanup controller refactor

--- controllers/data/bulk-download/index.js
+++ controllers/data/bulk-download/index.js
@@ -1,10 +1,15 @@
-const safePromise = require('../../../utility/safePromise');
-const { createTempDir } = require('./createTempDir');
-const cleanup = require('./cleanupTempDir');
-const streamArchive = require('./streamArchive');
-const fetchAndWriteData = require('./fetchAndWriteData');
 const initLog = require('../../../log-service');
 const moduleLogger = initLog('bulk-download');
+const {
+  createWorkspace,
+  fetchAllDatasets,
+  streamResponse,
+  scheduleCleanup,
+  sendValidationError,
+  sendWorkspaceError,
+  sendFetchError,
+  sendStreamError,
+} = require('./bulkDownloadUtils');
 
 // Validation function
 const validateRequest = (req, log) => {
@@ -55,64 +60,32 @@ const bulkDownloadController = async (req, res, next) => {
   // 1. Validate incoming request
   const validation = validateRequest(req, log);
   if (!validation.isValid) {
-    res.status(validation.statusCode).send(validation.message);
-    return next('validation failed');
+    return sendValidationError(res, next, validation);
   }
   const { shortNames } = validation;
-  log.debug('shortNames', shortNames);
 
-  // 2. create a guid-name temp directory
-  // TODO make call to createTempDir safe
-  let pathToTmpDir;
-  try {
-    pathToTmpDir = await createTempDir();
-  } catch (e) {
-    log.error('error creating directory', { error: e });
-    res.sendStatus(500);
-    return next('error creating temp directory');
+  // 2. Create workspace directory
+  const workspaceResult = await createWorkspace(log);
+  if (!workspaceResult.success) {
+    return sendWorkspaceError(res, next);
+  }
+  const { pathToTmpDir } = workspaceResult;
+
+  // 3. Fetch and write data for each requested dataset
+  const fetchResult = await fetchAllDatasets(pathToTmpDir, shortNames, req.reqId, log);
+  if (!fetchResult.success) {
+    return sendFetchError(res, next, fetchResult.error);
   }
 
-  log.debug('created temporory directory', pathToTmpDir);
-
-  // 3. fetch and write data for each requested dataset (csv + excel metadata sheet)
-  const [dataErr, result] = await fetchAndWriteData(
-    pathToTmpDir,
-    shortNames,
-    req.reqId,
-  );
-  if (dataErr) {
-    log.error('fetchAndWriteDataErr', dataErr);
-    if (dataErr.message === 'could not find dataset id for dataset name') {
-      res.status(400).send('no matching dataset');
-      return next('error finding dataset id');
-    } else {
-      res.status(500).send('error fetching data');
-      return next('error fetching data for bulk download');
-    }
+  // 4. Create zip and pipe response
+  const streamResult = await streamResponse(pathToTmpDir, res, log);
+  if (!streamResult.success) {
+    return sendStreamError(res, next);
   }
 
-  // 4. (once all data is fetched) create zip and pipe response
-
-  const safeStreamArchive = safePromise(streamArchive);
-  log.info('starting stream response');
-  const [streamError, streamResolve] = await safeStreamArchive(
-    pathToTmpDir,
-    res,
-  );
-  if (streamError) {
-    log.error('error streaming archive response');
-    res.status(500).send('error streaming archive');
-    return next('error streaming archive for bulk download');
-  } else {
-    log.debug('streamArchive resolved without error', { streamResolve });
-    next();
-  }
-
-  // by now, response has been sent: cleanup
-
-  // 5. initate clean-up of temp directory
-  const msg = await cleanup(pathToTmpDir);
-  moduleLogger.info('cleanup', { msg });
+  // 5. Schedule cleanup of temp directory
+  scheduleCleanup(pathToTmpDir, moduleLogger);
+  next();
 };
 
 module.exports = {
