64c3ad5772e4521a01b33c2ae6be6c084eaed41b
Howard Kim <hwk@uw.edu>
2025-09-25 12:18:57 -0700

    T014-T016

---
+++ endpointTester/tests/bulkDownload/simple-bulk-row-count-test.js
@@ -0,0 +1,10 @@
+/**
+ * Simple Bulk Row Count Test
+ * Test for /data/bulk-download-row-counts endpoint
+ * This will be migrated to the new framework format
+ */
+
+// Placeholder for migration - this will be populated during T017
+module.exports = {
+  // Test implementation will be added during framework migration
+};
