aac820f5eb6bd1210ba0e339bf290da82c4a2bf3
Howard Kim <hwk@uw.edu>
2025-09-29 11:54:22 -0700

    Add authentication

--- routes/collections.js
+++ routes/collections.js
@@ -1,12 +1,26 @@
 const router = require('express').Router();
+const passport = require('../middleware/passport');
 const collectionsController = require('../controllers/collections');
-const { validateCollectionsList, validateCollectionDetail } = require('../middleware/collectionsValidation');
+const {
+  validateCollectionsList,
+  validateCollectionDetail,
+} = require('../middleware/collectionsValidation');
 
 const asyncControllerWrapper = require('../errorHandling/asyncControllerWrapper');
 
 /////////////////// collections root route  ///////////////////
 
-router.get('/', validateCollectionsList, asyncControllerWrapper(collectionsController.list));
-router.get('/:id', validateCollectionDetail, asyncControllerWrapper(collectionsController.detail));
+router.get(
+  '/',
+  passport.authenticate(['jwt', 'headerapikey'], { session: false }),
+  validateCollectionsList,
+  asyncControllerWrapper(collectionsController.list),
+);
+router.get(
+  '/:id',
+  passport.authenticate(['jwt', 'headerapikey'], { session: false }),
+  validateCollectionDetail,
+  asyncControllerWrapper(collectionsController.detail),
+);
 
 module.exports = router;
