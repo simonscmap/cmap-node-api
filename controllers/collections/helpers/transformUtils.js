1501b7ac31d9675ee7d169d1367dc04031d6c32e
Howard Kim <hwk@uw.edu>
2026-01-28 00:25:02 +0000

    refactor dataset transofrm and include with listFollowed endpoint

---
+++ controllers/collections/helpers/transformUtils.js
@@ -0,0 +1,51 @@
+const mapCollectionFields = (row) => ({
+  id: row.id,
+  name: row.name,
+  description: row.description,
+  isPublic: Boolean(row.isPublic),
+  createdDate: row.createdDate ? new Date(row.createdDate).toISOString() : null,
+  modifiedDate: row.modifiedDate ? new Date(row.modifiedDate).toISOString() : null,
+  ownerName: row.ownerName,
+  ownerAffiliation: row.ownerAffiliation,
+  isOwner: Boolean(row.isOwner),
+  isFollowing: Boolean(row.isFollowing),
+  downloads: row.downloads,
+  views: row.views,
+  copies: row.copies,
+  followerCount: row.followerCount,
+});
+
+function transformResultsWithDatasets(results) {
+  const collectionsMap = new Map();
+
+  results.forEach((row) => {
+    const collectionId = row.id;
+
+    if (!collectionsMap.has(collectionId)) {
+      collectionsMap.set(collectionId, {
+        ...mapCollectionFields(row),
+        datasetCount: 0,
+        datasets: [],
+      });
+    }
+
+    const collection = collectionsMap.get(collectionId);
+
+    if (row.datasetShortName) {
+      collection.datasets.push({
+        datasetShortName: row.datasetShortName,
+        datasetLongName: row.datasetLongName,
+        isInvalid: Boolean(row.isInvalid),
+      });
+    }
+
+    collection.datasetCount = collection.datasets.length;
+  });
+
+  return Array.from(collectionsMap.values());
+}
+
+module.exports = {
+  mapCollectionFields,
+  transformResultsWithDatasets,
+};
